935562f066d708843fddd6430e1323a8ab1af9ad
McFadden <jmcfadden@misd.org>
2018-10-02 13:34:52 -0500

    setting up main for testing multiple choice input

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,18 @@
 
 int main()
 {
+	//basic text input
 	btin basic = btin();
+	//using a pointer so it can be changed
 	Input *in = &basic;
-	std::string ans = in->free_response("test question");
+	//choices vector
+	vector<string> choices = vector<string>();
+	//adding choices
+	choices.add("nah, I'm good")
+	choices.add("nah, I'm fine")
+	//querying user
+	std::string ans = in->multiple_choice("hey kids you want some <testing question>?",choices);
+	//returning answer
 	cout << "answer: " << ans << endl;
   	return 0;
 }
