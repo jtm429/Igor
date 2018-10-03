aa3b45ffc5338c0cd7ec998bac0df89efa085c46
jacob <jtm429@gmail.com>
2018-10-02 21:01:37 -0500

    fixed colors

--- include/basicTextInput.h
+++ include/basicTextInput.h
@@ -9,20 +9,20 @@ using namespace std;
 class btin : public Input {
 
 	private:
-		enum Format:int {reset=0,bold=1,underline=4,inverse=7,unbold=21,rmund=24,unverse=27};
+		// enum Format {reset=0,bold=1,underline=4,inverse=7,unbold=21,rmund=24,unverse=27};
 
-		enum Color:int {white=37,red=31,blue=34,magenta=35 };
-
-		//esc code gen
-		string setStyle(Color col,Format _4rmat)
-		{
-			string esc = "/033["+_4rmat+";"+col+"m";
-			return esc;
-		}
+		// enum Color {white=37,red=31,blue=34,magenta=35 };
+		//
+		// // //esc code gen
+		// // string setColor(Color col)
+		// // {
+		// // 	string esc = "/033["+rmat+";"+col+"m";
+		// // 	return esc;
+		// // }
 	public:
 		int multiple_choice(string question, vector<string> choices)
 		{
-			cout <<setStyle(blue,bold)<<question<<endl;
+			cout <<"\033[34m\033[1m"<<question<<"\033[0m"<<endl;
 			cout << "Enter the value to select" <<endl;
 			int size = choices.size();
 			for(int i = 0; i < size;i++)
