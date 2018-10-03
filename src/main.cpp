a177a2dc57207d6d90b8d774938b1b76d673d9be
McFadden <jmcfadden@misd.org>
2018-10-03 07:21:39 -0500

    created an ostream variable

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,24 @@
 using namespace std;
 int main()
 {
+	ostream logg = clog;
 	//basic text input
 	btin basic = btin();
-	cout << "creating basic text input" << endl;
+	logg << "creating basic text input" << endl;
 	//using a pointer so it can be changed
 	Input *in = &basic;
-	cout << "assigning it to input pointer" << endl;
+	logg << "assigning it to input pointer" << endl;
 	//choices vector
 	vector<string> choices = vector<string>();
-	cout << "creating vector for choices" << endl;
+	logg << "creating vector for choices" << endl;
 	//adding choices
 	choices.push_back("nah, I'm good");
 	choices.push_back("nah, I'm fine");
-	cout << "adding choices" << endl;
+	logg << "adding choices" << endl;
 	//querying user
 	string ans = ""+(in->multiple_choice("hey kids you want some <testing question>?",choices));
 	//returning answer
 	cout << "answer: " << ans << endl;
-	
+
   	return 0;
 }
