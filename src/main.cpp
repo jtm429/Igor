39f9735cad00549a6af992af5f8386fe4cd25f92
McFadden <jmcfadden@misd.org>
2018-10-08 06:48:00 -0500

    5

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,20 @@
 using namespace std;
 int main()
 {
-	ostream logg = cout;
+	// ostream logg = cout;
 	//basic text input
 	btin basic = btin();
-	logg << "creating basic text input" << endl;
+	// logg << "creating basic text input" << endl;
 	//using a pointer so it can be changed
 	Input *in = &basic;
-	logg << "assigning it to input pointer" << endl;
+	// logg << "assigning it to input pointer" << endl;
 	//choices vector
 	vector<string> choices = vector<string>();
-	logg << "creating vector for choices" << endl;
+	// logg << "creating vector for choices" << endl;
 	//adding choices
 	choices.push_back("nah, I'm good");
 	choices.push_back("nah, I'm fine");
-	logg << "adding choices" << endl;
+	// logg << "adding choices" << endl;
 	//querying user
 	string ans = ""+(in->multiple_choice("hey kids you want some <testing question>?",choices));
 	//returning answer
